521e017d7a870115c5b5134565c00be5c0910683
Utkarsh Nigam <contacts.utkarsh@gmail.com>
2020-07-26 23:55:39 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -13,9 +13,9 @@
     new Typed('.typed', {
       strings: typed_strings,
       loop: true,
-      typeSpeed: 100,
-      backSpeed: 50,
-      backDelay: 2000
+      typeSpeed: 60,
+      backSpeed: 30,
+      backDelay: 1200
     });
   }
 
